f9551ba2150d4597b0ffbdf57528b7c943506b91
IrfanAlvi <irfan.alvi@softcircles.org>
2024-04-20 21:09:20 +0500

    migrated whole template from CRA to Berry.

---
+++ src/redux/slices/customizationSlice.jsx
@@ -0,0 +1,63 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import config from '../../config';
+
+// import apiInstance from '../../utils/customAxios';
+
+const initialState = {
+  allOSList: [],
+
+  isOpen: [], // for active default menu
+  defaultId: 'default',
+  fontFamily: config.fontFamily,
+  borderRadius: config.borderRadius,
+  opened: true
+};
+
+const slice = createSlice({
+  name: 'customizationSlice',
+  initialState,
+  reducers: {
+    getOSList(state, action) {
+      state.allOSList = action.payload;
+    },
+
+    setIsOpen(state, action) {
+      state.isOpen = [action.payload];
+    },
+
+    setDrawer(state, action) {
+      state.opened = action.payload;
+    },
+    setMenu(state, action) {
+      state.isOpen = [action.payload];
+    },
+    setBorderRadius(state, action) {
+      state.borderRadius = action.payload;
+    },
+    setFontFamily(state, action) {
+      state.fontFamily = action.payload;
+    }
+  }
+});
+
+export const { setIsOpen, setDrawer, setBorderRadius, setFontFamily, setMenu } = slice.actions;
+export default slice.reducer;
+
+// export function getAllOSList(handleResponse) {
+//   return async (dispatch) => {
+//     try {
+//       const response = await apiInstance.get('v1/opticalsensors');
+
+//       dispatch(slice.actions.getOSList(response.data.data));
+//       if (handleResponse) {
+//         handleResponse();
+//       }
+//     } catch (error) {
+//       console.log('error here', error);
+//       if (handleResponse) {
+//         handleResponse();
+//       }
+//     }
+//   };
+// }
